ade185625a3df636d4e9a0278b826a169e9493bc
Abdillah <achmdabdil9@gmail.com>
2022-01-08 15:28:57 +0700

    done responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,26 +6,42 @@ export default function Home({ data }) {
   const bg = getImage(data.allFile.edges[0].node)
   const logo = getImage(data.allFile.edges[1].node)
   return (
-    <div className="flex h-screen font-roboto text-2xl font-medium">
-      <div className="hidden blue w-6/12 justify-center bg-gradient-to-bl from-red-start to-red-end text-white border md:flex">
+    <div className="flex flex-col h-screen font-roboto text-2xl font-medium md:flex-row">
+      <div className="hidden blue w-6/12 justify-center bg-gradient-to-bl from-red-start to-red-end text-white md:flex">
         <GatsbyImage
-          className="border h-full object-cover absolute"
+          className="h-full object-cover absolute"
           alt="background"
           image={bg}
         />
         <div className="w-9/12 flex flex-col justify-start relative">
           <div className="absolute mt-8">
             <GatsbyImage image={logo} alt="beam logo" />
-            <p className="mt-4 w-80">
+            <p className="mt-4 text-4xl w-4/5">
               Enjoy the Convenience of Beam Space Storage
             </p>
           </div>
         </div>
       </div>
-      <div className="blue w-6/12 flex justify-center bg-white-75">
-        <div className="w-3/5 my-10">
+      <div className="h-3/5 md:hidden">
+        <div className="h-full flex flex-col items-center relative">
+          <div className="mt-8 z-50">
+            <GatsbyImage image={logo} alt="beam logo" />
+            <p className="mt-4 w-80 text-white">
+              Enjoy the Convenience of Beam Space Storage
+            </p>
+          </div>
+          <GatsbyImage
+            className="h-full pt-5 absolute"
+            alt="background"
+            image={bg}
+          />
+          <StaticImage className="absolute top-36" src="../images/wave.svg" />
+        </div>
+      </div>
+      <div className="w-full h-full blue flex justify-center bg-white-75 md:w-6/12 ">
+        <div className="w-80 my-10 md:w-3/5 flex flex-col justify-center">
           <h2 className="text-red">Log in to Beam Space</h2>
-          <div className="flex flex-col gap-4 mt-5 text-base">
+          <div className="flex flex-col gap-5 mt-5 text-base">
             <button className="h-10 bg-white text-base flex text-center items-center justify-center align-middle rounded-lg font-medium">
               <StaticImage
                 className="w-6 mr-3"
@@ -46,7 +62,7 @@ export default function Home({ data }) {
               or login with your email
             </p>
           </div>
-          <div className="flex flex-col text-base gap-4">
+          <div className="flex flex-col text-base gap-5">
             <div className="flex flex-col gap-2">
               <p>
                 Email Address<span className="text-red">*</span>
